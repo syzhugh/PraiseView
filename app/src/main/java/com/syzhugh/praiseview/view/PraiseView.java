ee913095ad0b9642b4a45c95b088f2b55de44baf
syzhugh <syzhugh@163.com>
2017-11-06 10:50:17 +0800

    edit wrap state

--- app/src/main/java/com/syzhugh/praiseview/view/PraiseView.java
+++ app/src/main/java/com/syzhugh/praiseview/view/PraiseView.java
@@ -2,6 +2,7 @@ package com.syzhugh.praiseview.view;
 
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -12,6 +13,7 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.OvershootInterpolator;
+import com.syzhugh.praiseview.R;
 
 /**
  * Created by syzhu on 2017/11/5.
@@ -19,39 +21,39 @@ import android.view.animation.OvershootInterpolator;
 
 public class PraiseView extends View {
 
-  public static final int PRAISESTATE_NO = 0;
-  public static final int PRAISESTATE_YES = 1;
+  private final String TAG = "Test";
 
   private final float c = 0.551915024494F;
 
-  private final Paint mPaint;
+  public static final int PRAISESTATE_NO = 0;
+  public static final int PRAISESTATE_YES = 1;
 
+  private int mPraiseState;
+
+
+  private final Paint mPaint;
   private float mRadius;
   private float mCurrentRadius;
   private float mOffset;
-  private PointF mCenterPoint;
+  private Path mHeartPath;
+  private ValueAnimator mAnimator;
 
-  private int mPraiseState;
+  private int mDuration;
+  private int mColorPraised;
+  private int mColorUnPraised;
 
   private PointF mTopPointA;
   private PointF mTopPointB;
   private PointF mTopPoint;
-
   private PointF mBottomPointA;
   private PointF mBottomPointB;
   private PointF mBottomPoint;
-
   private PointF mLeftPointA;
   private PointF mLeftPointB;
   private PointF mLeftPoint;
-
   private PointF mRightPointA;
   private PointF mRightPointB;
   private PointF mRightPoint;
-
-  private ValueAnimator mAnimator;
-  private int mDuration;
-  private Path mHeartPath;
 
   public PraiseView(Context context) {
     this(context, null);
@@ -63,13 +65,15 @@ public class PraiseView extends View {
 
   public PraiseView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
     super(context, attrs, defStyleAttr);
-    mCenterPoint = new PointF(540, 960);
-    mRadius = 100;
-    mCurrentRadius = mRadius;
+    TypedArray typedArray = getResources().obtainAttributes(attrs, R.styleable.PraiseView);
+    mColorUnPraised = typedArray
+        .getColor(R.styleable.PraiseView_praiseview_color_unpraised, Color.BLACK);
+    mColorPraised = typedArray.getColor(R.styleable.PraiseView_praiseview_color_praised, Color.RED);
+    mDuration = typedArray.getInt(R.styleable.PraiseView_praiseview_duration, 300);
+    typedArray.recycle();
 
     mPaint = new Paint();
     mAnimator = new ValueAnimator();
-    mDuration = 300;
 
     mHeartPath = new Path();
     mTopPointA = new PointF();
@@ -87,17 +91,29 @@ public class PraiseView extends View {
   }
 
   @Override
+  protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+    super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    int realWidth = getMeasuredWidth() - getPaddingLeft() - getPaddingRight();
+    int realHeight = getMeasuredHeight() - getPaddingTop() - getPaddingBottom();
+    mRadius = Math.min(realWidth, realHeight) / 2;
+    mCurrentRadius = mRadius;
+  }
+
+  @Override
   protected void onDraw(Canvas canvas) {
-    canvas.translate(mCenterPoint.x, mCenterPoint.y);
+    //全局居中 绘制
+    canvas.translate(getWidth() / 2, getHeight() / 2);
+    //画心
     drawHeart(canvas, mCurrentRadius);
+//    //显示控制点
 //    showControllPoint(canvas);
   }
 
   private void drawHeart(Canvas canvas, float radius) {
     initControlPoint(radius);
-    mPaint.setColor(mPraiseState == PRAISESTATE_NO ? Color.BLACK : Color.RED);
+    mPaint.setColor(mPraiseState == PRAISESTATE_NO ? mColorUnPraised : mColorPraised);
     mPaint.setStyle(mPraiseState == PRAISESTATE_NO ? Paint.Style.STROKE : Paint.Style.FILL);
-    mPaint.setStrokeWidth(15);
+    mPaint.setStrokeWidth(mRadius / 10);
     mPaint.setStrokeJoin(Paint.Join.ROUND);
     mPaint.setAntiAlias(true);
 
@@ -118,7 +134,7 @@ public class PraiseView extends View {
 
   private void showControllPoint(Canvas canvas) {
     mPaint.setColor(Color.BLACK);
-    mPaint.setStrokeWidth(3);
+    mPaint.setStrokeWidth(10);
     mPaint.setAntiAlias(true);
     mPaint.setStyle(Paint.Style.FILL);
     canvas.drawPoint(mTopPointA.x, mTopPointA.y, mPaint);
@@ -137,19 +153,15 @@ public class PraiseView extends View {
 
   private void initControlPoint(float radius) {
     mOffset = c * radius;
-
     mTopPointA.set(-mOffset, -radius);
     mTopPointB.set(mOffset, -radius);
     mTopPoint.set(0, -radius * 0.5f);
-
     mRightPointA.set(radius, -mOffset);
     mRightPointB.set(radius * 0.9f, mOffset);
     mRightPoint.set(radius, 0);
-
     mBottomPointA.set(-mOffset, radius * 0.7f);
     mBottomPointB.set(mOffset, radius * 0.7f);
     mBottomPoint.set(0, radius);
-
     mLeftPointA.set(-radius, -mOffset);
     mLeftPointB.set(-radius * 0.9f, mOffset);
     mLeftPoint.set(-radius, 0);
